113691077bcdcaff2540ba0282180061ad43c117
Sveta <Sveta@KOMP>
2021-12-15 22:41:09 +0300

    Добавьте файлы проекта.

---
+++ 23/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _23
+{
+    class Program
+    {
+        static public int x;
+        static int factorial = 1;
+        static void ArrayFactorial(int x)
+        {
+            Console.WriteLine("Метод ArrayFactorial начал работу!\n");            
+            for (int a = 1; a <= x; a++)
+            {
+                Console.WriteLine("Выводится последовательность чисел: "+a+"\n");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Метод ArrayFactorial вывел последовательность чисел на консоль!\n");
+        }
+        static int Factorial(int x)
+        {
+            Console.WriteLine("Расчет факториала методом Factorial начался!\n");
+            for (int a = 1; a <= x; a++)
+            {
+                factorial = factorial * a;
+                Console.WriteLine("Последовательный расчет факториала : "+factorial+"\n");
+                Thread.Sleep(400);
+            }
+            Console.WriteLine("Факториал числа " +x+"! равен " + factorial+"\n");
+            return (factorial);
+        }
+        static async void Async()
+        {
+            await Task.Run(() => ArrayFactorial(x));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число, факториал которого надо вычислить:  ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Async();
+            Factorial(x);
+            Console.WriteLine("Метод Main окончил работу!!!");            
+            Console.ReadKey();
+        }
+
+    }
+}
